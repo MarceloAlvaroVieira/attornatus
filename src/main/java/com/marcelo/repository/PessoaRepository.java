268756ee0b7609f0e871fab72e5c815cca5843a4
MarceloAlvaroVieira <marceloalvarovieira@gmail.com>
2023-01-11 17:53:48 -0300

    feat(Repositories): Criação dos Repositories da aplicação

---
+++ src/main/java/com/marcelo/repository/PessoaRepository.java
@@ -0,0 +1,7 @@
+package com.marcelo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.marcelo.model.Pessoa;
+
+public interface PessoaRepository extends JpaRepository<Pessoa, Long>{}
