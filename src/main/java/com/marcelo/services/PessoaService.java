10e3e709024c669b460b5310b3ce13cbb3bf2faa
MarceloAlvaroVieira <marceloalvarovieira@gmail.com>
2023-01-12 09:51:12 -0300

    feat(Services): Funções dos Services implementadas

--- src/main/java/com/marcelo/services/PessoaService.java
+++ src/main/java/com/marcelo/services/PessoaService.java
@@ -1,35 +1,71 @@
 package com.marcelo.services;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.marcelo.dto.PessoaDTO;
+import com.marcelo.exception.ResourceNotFoundException;
 import com.marcelo.model.Pessoa;
+import com.marcelo.repository.PessoaRepository;
 
 @Service
 public class PessoaService {
-    
-    public ResponseEntity<PessoaDTO> create(Pessoa pessoa) {
-        return null;
+
+    @Autowired
+    PessoaRepository repository;
+
+    public ResponseEntity<PessoaDTO> create(PessoaDTO pessoaDTO) {
+        Pessoa pessoa = repository.save(pessoaDTO.toPessoa());
+        return ResponseEntity.ok(new PessoaDTO().fromPessoa(pessoa));
     }
 
     public ResponseEntity<List<PessoaDTO>> read() {
-        return null;
+
+        List<Pessoa> pessoas = repository.findAll();
+        List<PessoaDTO> pessoasDTO = new ArrayList<>();
+
+        pessoas.forEach((pessoa) -> {
+            pessoasDTO.add(new PessoaDTO().fromPessoa(pessoa));
+        });
+
+        return ResponseEntity.ok(pessoasDTO);
     }
 
-    public ResponseEntity<List<PessoaDTO>> getById(int id) {
-        return null;
-    }
-    
-    public ResponseEntity<PessoaDTO> update(int id, Pessoa pessoa) {
-        return null;
+    public ResponseEntity<PessoaDTO> getById(Long id) {
+
+        Pessoa pessoa = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Pessoa não encontrada"));
+
+        return ResponseEntity.ok(new PessoaDTO().fromPessoa(pessoa));
     }
 
-    public ResponseEntity<Map<String, Boolean>> delete(int id) {
-        return null;
+    public ResponseEntity<PessoaDTO> update(Long id, PessoaDTO pessoaDTO) {
+
+        Pessoa pessoa = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Pessoa não encontrada"));
+
+        pessoa = repository.save(pessoaDTO.toPessoa());
+
+        return ResponseEntity.ok(new PessoaDTO().fromPessoa(pessoa));
+    }
+
+    public ResponseEntity<Map<String, Boolean>> delete(Long id) {
+
+        Pessoa pessoa = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Pessoa não encontrada"));
+
+        repository.delete(pessoa);
+
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+
+        return ResponseEntity.ok(response);
     }
 
 }
